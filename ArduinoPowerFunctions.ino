f78e325b08431034b2e0cdb117365cfff216d99e
David Wegmuller <david@wegmuller.org>
2014-06-07 17:07:34 -0700

    Chnanged the PWM frequency to create a signal that is more friendly to the Lego servo.

--- ArduinoPowerFunctions.ino
+++ ArduinoPowerFunctions.ino
@@ -2,13 +2,14 @@
 
 // Pinout
 #define POT_INPUT A1
-#define PWM_OUT1 9
+#define PWM_OUT1 6
 #define CTRL1 7
 #define CTRL2 8
 
-void setup() {
-  // put your setup code here, to run once:
-
+void setup() 
+{
+  TCCR0B &= ~(CS02 | CS01 |CS00);
+  TCCR0B |= CS00;
 }
 
 void loop() 
@@ -21,6 +22,7 @@ void loop()
     digitalWrite(CTRL1, HIGH);
     digitalWrite(CTRL2, LOW);
     analogWrite(PWM_OUT1, 255 - (pot >>1));
+    // speed up the PWM on pin 6
   }
   else
   {
@@ -30,6 +32,7 @@ void loop()
       digitalWrite(CTRL2, HIGH);
       digitalWrite(CTRL1, LOW);
       analogWrite(PWM_OUT1, (pot - 513) >>1);
+      // speed up the PWM on pin 6
     }
     else
     {
