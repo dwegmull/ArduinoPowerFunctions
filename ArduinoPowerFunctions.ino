59b9379d226de7e8350eabdd470fd9c910f24863
David Wegmuller <david@wegmuller.org>
2014-07-12 12:25:30 -0700

    Added a second output, so steering and drive can be controlled. Both take their input from a single potentiometer, for now.

--- ArduinoPowerFunctions.ino
+++ ArduinoPowerFunctions.ino
@@ -2,9 +2,13 @@
 
 // Pinout
 #define POT_INPUT A1
-#define PWM_OUT1 6
-#define CTRL1 7
-#define CTRL2 8
+#define PWM_OUT_STEER 6
+#define CTRL1_STEER 7
+#define CTRL2_STEER 8
+#define PWM_OUT_DRIVE 9
+#define CTRL1_DRIVE 2
+#define CTRL2_DRIVE 3
+
 
 // Range macro: like a switch, but with ranges instead of single values!
 #define RANGE(V, L, U, R) if((L <= V) && (U >= V)) return R;
@@ -34,26 +38,33 @@ void loop()
   if(pot < 500)
   {
     // forward
-    digitalWrite(CTRL1, HIGH);
-    digitalWrite(CTRL2, LOW);
-    analogWrite(PWM_OUT1, check_Range(255 - (pot >>1)));
-    // speed up the PWM on pin 6
+    digitalWrite(CTRL1_STEER, HIGH);
+    digitalWrite(CTRL2_STEER, LOW);
+    analogWrite(PWM_OUT_STEER, check_Range(255 - (pot >>1)));
+
+    digitalWrite(CTRL1_DRIVE, HIGH);
+    digitalWrite(CTRL2_DRIVE, LOW);
+    analogWrite(PWM_OUT_DRIVE, (255 - (pot >>1)));
   }
   else
   {
     if(pot > 524)
     {
       // Reverse
-      digitalWrite(CTRL2, HIGH);
-      digitalWrite(CTRL1, LOW);
-      analogWrite(PWM_OUT1, check_Range((pot - 513) >>1));
-      // speed up the PWM on pin 6
+      digitalWrite(CTRL2_STEER, HIGH);
+      digitalWrite(CTRL1_STEER, LOW);
+      analogWrite(PWM_OUT_STEER, check_Range((pot - 513) >>1));
+
+      digitalWrite(CTRL2_DRIVE, HIGH);
+      digitalWrite(CTRL1_DRIVE, LOW);
+      analogWrite(PWM_OUT_DRIVE, ((pot - 513) >>1));
     }
     else
     {
       // Stopped
-      analogWrite(PWM_OUT1, 0);
-      
+      analogWrite(PWM_OUT_STEER, 0);
+
+      analogWrite(PWM_OUT_DRIVE, 0);      
     }
   }
 }
